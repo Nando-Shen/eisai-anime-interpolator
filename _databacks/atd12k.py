30d6a340124cba2f06728aa8d022894904d283d8
shenjiaming <shenjiaming@rd.netease.com>
2023-02-21 17:57:12 +0800

    f

--- _databacks/atd12k.py
+++ _databacks/atd12k.py
@@ -1,6 +1,4 @@
 
-
-
 from _util.util_v0 import * ; import _util.util_v0 as uutil
 from _util.twodee_v0 import * ; import _util.twodee_v0 as u2d
 from _util.pytorch_v0 import * ; import _util.pytorch_v0 as utorch
@@ -20,33 +18,29 @@ class DatabackendATD12k:
         elif isinstance(idx, str):
             bn = idx
         else:
-            assert 0, f'{idx=} not understood'
+            assert 0, '{} not understood'.format(idx)
         return {
             'bn': bn,
             'images': [
                 I(self.get_fn(bn, i))
                 for i in range(3)
             ],
-            'flows': torch.tensor(load(f'{self.fn}/preprocessed/rfr_540p/{bn}.pkl')).flip(dims=(0,1)),
+            'flows': torch.tensor(load('{}/preprocessed/rfr_540p/{}.pkl'.format(self.fn,bn))).flip(dims=(0,1)),
         }
     def get_fn(self, bn, fidx):
         tt,tid = bn.split('/')
         if tt=='test':
-            dn = f'{self.dn}/raw/test_2k_{self.test_source}'
+            dn = '{}/raw/test_2k_{}'.format(self.dn, self.test_source)
             # ext = 'png' if self.test_source=='540p' else 'jpg'
             ext = 'jpg'
         else:
-            dn = f'{self.dn}/raw/train_10k'
+            dn = '{}/raw/train_10k'.format(self.dn)
             ext = 'jpg'
-        return f'{dn}/{tid}/frame{fidx+1}.{ext}'
+        return '{}/{}/frame{}.{}'.format(dn,tid,fidx+1,ext)
     def get_bns(self):
         return sorted([
-            f'test/{dn}'
-            for dn in os.listdir(f'{self.dn}/raw/test_2k_{self.test_source}')
-            if os.path.isdir(f'{self.dn}/raw/test_2k_{self.test_source}/{dn}')
+            'test/{}'.format(dn)
+            for dn in os.listdir('{}/raw/test_2k_{}'.format(self.dn, self.test_source))
+            if os.path.isdir('{}/raw/test_2k_{}/{}'.format(self.dn, self.test_source, dn))
         ])
 
-
-
-
-        
