99dfc9891ecde2696e4e5ece8132d4027fe85615
shenjiamingnus <e1221616@u.nus.edu>
2024-07-22 23:59:44 +0800

    train on animerun

---
+++ _databacks/animerun.py
@@ -0,0 +1,60 @@
+
+from _util.util_v0 import * ; import _util.util_v0 as uutil
+from _util.twodee_v0 import * ; import _util.twodee_v0 as u2d
+from _util.pytorch_v0 import * ; import _util.pytorch_v0 as utorch
+
+class DatabackendATD12k:
+    def __init__(self):
+        self.dn = '/home/kuhu6123/jshe2377/AnimeRun/AnimeRun/'
+        self.fn = '/home/kuhu6123/jshe2377/AnimeRun/AnimeRun/'
+        # self.test_source = '540p'
+        self.bns = np.array(self.get_bns(), dtype=np.string_)
+        return
+    def __len__(self):
+        return len(self.bns)
+    def __getitem__(self, idx):
+        if isinstance (idx, int):
+            bn = str(self.bns[idx], encoding='utf-8')
+        elif isinstance(idx, str):
+            bn = idx
+        else:
+            assert 0, '{} not understood'.format(idx)
+        tt, tid = bn.split('/')
+        flow0 = torch.from_numpy(load('{}/{}/guide_flo13.npy'.format(self.fn, self.get_ffn(bn))))
+        flow1 = torch.from_numpy(load('{}/{}/guide_flo31.npy'.format(self.fn, self.get_ffn(bn))))
+        # print(flow1.size())
+        return {
+            'bn': bn,
+            'images': [
+                I(self.get_fn(bn, i))
+                for i in range(3)
+            ],
+            'flows': torch.stack([flow0,flow1], dim=1),
+            'fn': tid
+        }
+
+    def get_ffn(self, bn):
+        tt, tid = bn.split('/')
+        if tt=='test':
+            fff = 'test'
+        else:
+            fff = 'train'
+        return '{}/contour_label/{}'.format(fff, tid)
+
+
+    def get_fn(self, bn, fidx):
+        tt,tid = bn.split('/')
+        if tt=='test':
+            dn = '{}/test/contourcopy'.format(self.dn)
+            ext = 'jpg'
+        else:
+            dn = '{}/train/contourcopy'.format(self.dn)
+            ext = 'jpg'
+        return '{}/{}/frame{}.{}'.format(dn,tid,fidx+1,ext)
+    def get_bns(self):
+        return sorted([
+            'test/{}'.format(dn)
+            for dn in os.listdir('{}/test/contourcopy'.format(self.dn))
+            if os.path.isdir('{}/test/contourcopy/{}'.format(self.dn, dn))
+        ])
+
---
+++ _train/frame_interpolation/train-animerun.py
@@ -0,0 +1,75 @@
+
+
+
+
+import sys
+if sys.path[0]!='': sys.path.insert(0, '')
+
+from _util.util_v0 import * ; import _util.util_v0 as util
+from _util.twodee_v0 import * ; import _util.twodee_v0 as u2d
+from _util.pytorch_v0 import * ; import _util.pytorch_v0 as utorch
+
+
+ap = argparse.ArgumentParser()
+ap.add_argument('dataset', type=str)
+ap.add_argument('output', type=str)
+args = ap.parse_args()
+
+
+#################### train ####################
+
+from _train.frame_interpolation.models.trainmodel import TrainModel
+model = TrainModel()
+# model = TrainModel().load_from_checkpoint \
+#     ('temp/training_demo_output/checkpoints/epoch=0019-val_lpips=0.097225.ckpt')
+
+# from _train.frame_interpolation.datasets.rrldextr import Datamodule
+from _train.frame_interpolation.datasets.animerun import Datamodule
+dm = Datamodule(args.dataset, bs=4)
+
+trainer = pl.Trainer(
+    precision=16,
+    # fast_dev_run=True,
+    # limit_train_batches=4,
+    # limit_val_batches=4,
+    # limit_test_batches=4,
+    gradient_clip_val=1.0,
+    max_epochs=2,
+
+    default_root_dir=mkdir(args.output),
+
+    accelerator='gpu',
+    gpus=1,
+    accumulate_grad_batches=8,
+
+    callbacks=[pl.callbacks.ModelCheckpoint(
+        monitor='val_lpips',
+        mode='min',
+        filename='{epoch:04d}-{val_lpips:0.6f}',
+        save_top_k=8,
+        dirpath=mkdir(f'{args.output}/checkpoints'),
+        save_last=True,
+    )],
+    logger=[pl.loggers.TensorBoardLogger(
+        mkdir(f'{args.output}/logs'),
+        name='tensorboard',
+        version=0,
+        log_graph=False,
+        default_hp_metric=True,
+        prefix='',
+    )],
+)
+
+trainer.fit(
+    model,
+    datamodule=dm,
+)
+
+
+
+
+
+
+
+
+
